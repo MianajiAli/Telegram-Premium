f79918fd5d457e15a7686b4c20f65d4afecbd20f
Ali <alisarabi304@gmail.com>
2024-02-01 02:46:25 +0330

    add webhooks

---
+++ index.js
@@ -0,0 +1,25 @@
+function webhook() {
+  const msg =
+    document.getElementById("message").value +
+    "|" +
+    document.getElementById("message1").value;
+
+  console.log(25);
+  discord_message(
+    "https://discord.com/api/webhooks/1202381982873419796/OAox5_Kn88eArM6QyxWYb9vkt65lwR6l5LLeZVpbooMTc6R3pfyqruy1Fdi_LGlp9dHl",
+    msg
+  );
+  console.log(252);
+}
+
+function discord_message(webHookURL, message) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", webHookURL, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(
+    JSON.stringify({
+      content: message,
+      username: "AI",
+    })
+  );
+}
